a3add3fdd4a9b7561ba12adf8a444e9d1369faec
omarkaka <41745215+omarkaka@users.noreply.github.com>
2018-08-03 17:09:15 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const prefix = '!!'
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`§help|Monster Bot|2018`,"http://twitch.tv/S-F")
+client.user.setGame(`!!help|Monster Bot|2018`,"http://twitch.tv/S-F")
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
@@ -289,8 +289,6 @@ client.on('message', message => {
              Admin Commands
 ╚[❖════════════❖]╝
 
- ❖ !!ban = يطرد شخص من سيرفر
-
  ❖ !!kick = يحظر شخص من سيرفر
 
  ❖ !!mutechannel = يقفل روم الي تبيه لازم تكون في روم الي تبي تقفله
@@ -500,33 +498,4 @@ client.on('message',async message => {
   }
 });
 
-  client.on('message', message => {
-  if (message.author.codes) return;
-  if (!message.content.startsWith(prefix)) return;
-
-  let command = message.content.split(" ")[0];
-  command = command.slice(prefix.length);
-
-  let args = message.content.split(" ").slice(1);
-
-  if (command == "!!ban") {
-               if(!message.channel.guild) return message.reply('** This command only for servers**');
-         
-  if(!message.guild.member(message.author).hasPermission("BAN_MEMBERS")) return message.reply("**انت لا تملك الصلاحيات المطلوبه**");
-  if(!message.guild.member(client.user).hasPermission("BAN_MEMBERS")) return message.reply("**I Don't Have ` BAN_MEMBERS ` Permission**");
-  let user = message.mentions.users.first();
-  
-  if (message.mentions.users.size < 1) return message.reply("**منشن شخص**");
-  if (!message.guild.member(user)
-  .bannable) return message.reply("**يجب ان تكون رتبة البوت اعلي من رتبه الشخص المراد تبنيدة**");
-
-
-  message.guild.member(user).ban(7, user);
-
-message.channel.send(`**:white_check_mark: ${user.tag} banned from the server ! :airplane: **  `)
-
-}
-});
-  
- 
 client.login(process.env.BOT_TOKEN);
