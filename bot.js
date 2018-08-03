df8debdcc58badb6b9528d210ad1fa1681c86ee1
omarkaka <41745215+omarkaka@users.noreply.github.com>
2018-08-03 11:29:31 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -355,29 +355,4 @@ message.channel.send(**${message.author.username}, your 💳 balance is \`${user
 }
 })
 
-client.on('message', async message => {
-    let amount = 250;
-    if(message.content.startsWith(prefix + "%daily")) {
-    if(message.author.bot) return;
-    if(coolDown.has(message.author.id)) return message.channel.send(⏱ | ${message.author.username}, your daily 💴 credits refreshes in ``1 Day``.);
-    
-    let userData = credits[message.author.id];
-    let m = userData.credits + amount;
-    credits[message.author.id] = {
-    credits: m
-    };
-
-    fs.writeFile("./creditsCode.json", JSON.stringify(userData.credits + amount), (err) => {
-    if (err) console.error(err);
-    });
-    
-    message.channel.send(🏧 | ${message.author.username}, you received your 💴 ${amount} credits!**`).then(() => {
-        coolDown.add(message.author.id);
-    });
-
-    setTimeout(() => {
-       coolDown.remove(message.author.id);
-    },86400000);
-    }
-});
 client.login(process.env.BOT_TOKEN);
