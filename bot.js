218ae73bb64ecee8a422cf5d3a07aeb3e085d733
omarkaka <41745215+omarkaka@users.noreply.github.com>
2018-08-05 13:22:58 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -556,5 +556,76 @@ function timeCon(time) {
     return `${days > 0 ? `${days}:` : ''}${(hours || days) > 0 ? `${hours}:` : ''}${minutes}:${seconds}`
 }
 
+  client.on("ready", () => {
 
-client.login(process.env.BOT_TOKEN);
+    var guild;
+
+    while (!guild)
+
+        guild = client.guilds.get("475372116552974337 - Server id");
+
+    guild.fetchInvites().then((data) => {
+
+        data.forEach((Invite, key, map) => {
+
+            var Inv = Invite.code;
+
+            dat[Inv] = Invite.uses;
+
+        });
+
+    });
+
+});
+
+
+client.on("guildMemberAdd", (member) => {
+
+    let channel = member.guild.channels.get("475373758408818688 - Room id");
+
+    if (!channel) {
+
+        console.log("!the channel id it's not correct");
+
+        return;
+
+    }
+
+    if (member.id == client.user.id) {
+
+        return;
+
+    }
+
+    console.log('-');
+
+    var guild;
+
+    while (!guild)
+
+        guild = client.guilds.get("475372116552974337 - Server id");
+
+    guild.fetchInvites().then((data) => {
+
+        data.forEach((Invite, key, map) => {
+
+            var Inv = Invite.code;
+
+            if (dat[Inv])
+
+                if (dat[Inv] < Invite.uses) {
+
+ channel.send(`تم دعوته بواسطة  ${Invite.inviter} `) ;       
+
+ }
+
+            dat[Inv] = Invite.uses;
+
+       
+
+       });
+
+    });
+
+});
+  client.login(process.env.BOT_TOKEN);
